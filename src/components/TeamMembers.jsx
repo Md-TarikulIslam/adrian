c0602648794d7836dfb0a70635a7a8bde87ac564
MdTarikulIslam <nayon1820@gmail.com>
2023-10-30 20:11:57 +0600

    Team member  section is added

---
+++ src/components/TeamMembers.jsx
@@ -0,0 +1,53 @@
+import img from '../assets/images/team/1.jpg'
+import img2 from '../assets/images/team/2.jpg'
+import img3 from '../assets/images/team/3.jpg'
+import img4 from '../assets/images/team/4.jpg'
+
+import '../assets/css/TeamMember.css'
+export const TeamMembers = () => {
+const allData =[
+    {
+        id:1,
+        img:img,
+        name:"Jonathon Andrew",
+        designation:"Founder"
+    },
+    {
+        id:2,
+        img:img2,
+        name:"Adipisci Velit",
+        designation:"Photographer"
+    },
+    {
+        id:3,
+        img:img3,
+        name:"John Fexit",
+        designation:"Manager"
+    },
+    {
+        id:4,
+        img:img4,
+        name:"John Fexit",
+        designation:"creative director"
+    },
+]
+    
+  return (
+    <div className="max-area">
+            <p className='about'>Team members</p>
+                <div className='grid-2'>
+                    {
+                        allData.map((data)=>{
+                            return(
+                                <div key={data.id}>
+                                    <img className='team-img' src={data.img} alt="" />
+                                    <p className='team-name'>{data.name}</p>
+                                    <p className='team-designation'>{data.designation}</p>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+        </div>
+  )
+}
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,6 +1,7 @@
 import AboutUs from "../components/AboutUs"
 import Category from "../components/Category"
 import Hero from "../components/Hero"
+import { TeamMembers } from "../components/TeamMembers"
 
 const Homepage = () => {
     return (
@@ -8,6 +9,7 @@ const Homepage = () => {
             <Hero />
             <Category />
             <AboutUs />
+            <TeamMembers />
         </div>
     )
 }
